0139e58ce25f3d88421dc8fb897ac8dd40100756
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-16 00:55:07 +0330

    added main page and it's style

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,39 @@
+import Dropzone from "../components/Dropzone"
+
+import styles from "./MainPage.module.css"
+
+import Navbar from "../components/Navbar"
+import Result from "../components/Result"
+
+import { useState } from "react"
+
+const MainPage = () => {
+  const [file, setFile] = useState(null)
+
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <h2 className={styles.title}>عکس مورد نظرت رو آپلود کن</h2>
+        <p className={styles.description}>
+          تصاویر خود را بکشید و رها کنید یا برای انتخاب کلیک کنید. ما از همه
+          قالب‌های رایج تصویر پشتیبانی می‌کنیم فرایند آپلود را به صورت آنی با
+          پیش‌نمایش‌ ارائه می‌دهیم
+        </p>
+        <Dropzone file={file} setFile={setFile} />
+        {file && <Result file={file} setFile={setFile} />}
+
+        {/* {uploadProgress > 0 && (
+        <div className="progress-bar">
+          <div
+          className="progress"
+          style={{ width: `${uploadProgress}%` }}
+          ></div>
+          </div>
+      )} */}
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
