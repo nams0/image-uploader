04a4ed13c84777738f5dd18dc580d2198c2bf644
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-16 01:06:30 +0330

    handled upload, fetching and errors

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 import { useDropzone } from "react-dropzone"
 
 import { useDispatch, useSelector } from "react-redux"
@@ -45,6 +43,74 @@ function Dropzone({ file, setFile }) {
       }
     },
   })
+
+  const handleUpload = async () => {
+    if (!file) return
+
+    dispatch(setUploading(true))
+    dispatch(setError(""))
+
+    const formData = new FormData()
+    formData.append("image", file)
+
+    try {
+      const response = await api.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (ProgressEvent) => {
+          const percentCompleted = Math.round(
+            (ProgressEvent.loaded * 100) / ProgressEvent.total
+          )
+          dispatch(setUploadProgress(percentCompleted))
+        },
+      })
+
+      dispatch(setFileUrl(response.url))
+      dispatch(setDownloadUrl(response.downloadUrl))
+      dispatch(setFileInfo(response.fileInfo))
+      dispatch(setUploaded(true))
+    } catch (err) {
+      if (err.response) {
+        if (err.response.status === 429) {
+          dispatch(
+            setError(
+              "تعداد درخواست‌های شما زیاد بوده است. لطفاً یک ساعت دیگر تلاش کنید"
+            )
+          )
+        } else {
+          dispatch(setError("خطا در آپلود فایل: " + err.response.error))
+        }
+      } else {
+        dispatch(setError("خطا در اتصال به سرور"))
+      }
+    } finally {
+      dispatch(setUploading(false))
+    }
+  }
+  return (
+    <div>
+      <div {...getRootProps()} className={styles.dropzone}>
+        <LuUpload className={styles.uploadIcon} />
+        <input {...getInputProps()} />
+        <p className={styles.dropzoneText}>عکس را اینجا رها کنید</p>
+        <p>یا برای انتخاب کلیک کنید</p>
+        <div className={styles.selectButton}>
+          <CiImageOn strokeWidth={0.5} />
+          <p>انتخاب عکس</p>
+        </div>
+        <p className="limit-info">حداکثر حجم فایل: ۳ مگابایت</p>
+      </div>
+
+      {store.error && <p className="error">{store.error}</p>}
+
+      {file && !store.uploaded && (
+        <button onClick={handleUpload} disabled={store.uploading}>
+          {store.uploading ? "در حال آپلود..." : "آپلود عکس"}
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default Dropzone
