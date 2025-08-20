cd39a562da5757bdeff4ca4f54bbb93453908054
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-20 14:24:29 +0330

    moved album container to Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react"
 
 import styles from "./Result.module.css"
 
+import { BsImages } from "react-icons/bs"
+
 function Result({ file }) {
   const store = useSelector((store) => store.uploader)
   const [previewUrl, setPreviewUrl] = useState("") // State for the preview URL
@@ -22,45 +24,35 @@ function Result({ file }) {
 
   return (
     <div>
-      <h4>عکس آپلود شده</h4>
-      <div className={styles.cardContainer}>
-        <div className={styles.card}>
-          {previewUrl && <img src={previewUrl} alt="عکس آپلود شده" />}
-        </div>
-        {store.uploaded && (
-          <div className="result">
-            <h3>آپلود موفق!</h3>
-            <div className="file-info">
-              <p>نام فایل: {store.fileInfo.originalName}</p>
-              <p>
-                حجم فایل: {(store.fileInfo.size / (1024 * 1024)).toFixed(2)}{" "}
-                مگابایت
-              </p>
-              <p>
-                تاریخ آپلود:{" "}
-                {new Date(store.fileInfo.uploadDate).toLocaleString("fa-IR")}
-              </p>
+      <div className={styles.albumContainer}>
+        <div className={styles.albumTitle}>
+          <div className={styles.albumTitleContainer}>
+            <div className={styles.albumTitleIcon}>
+              <BsImages />
             </div>
-
-            <p>لینک عکس:</p>
-            <input
-              type="text"
-              value={store.fileUrl}
-              readOnly
-              onClick={(e) => e.target.select()}
-            />
-            <button
-              onClick={() => navigator.clipboard.writeText(store.fileUrl)}
-            >
-              کپی لینک
-            </button>
-            <p>
-              <a href={store.fileUrl} target="_blank" rel="noopener noreferrer">
-                مشاهده عکس
-              </a>
-            </p>
+            <div className={styles.albumInfo}>
+              <p>عکس های شما</p>
+              <p className={styles.albumInfoCount}>هنوز عکسی آپلود نشده</p>
+            </div>
           </div>
-        )}
+
+          <button>حذف همه</button>
+        </div>
+
+        <div className={styles.emptyResult}>
+          <div className={styles.emptyResultIcon}>
+            <BsImages />
+          </div>
+          <div className={styles.emptyResultText}>
+            <p className={styles.emptyResultTitle}>هنوز عکسی آپلود نشده</p>
+            <p>اولین عکستو به کمک بخش بالا آپلود کن</p>
+            <div className={styles.emptyResultFeatures}>
+              <p>✓ بگیر و رها کن</p>
+              <p>✓ انتخاب چندین عکس</p>
+              <p>✓ ردیابی فرایند</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,8 +7,6 @@ import Result from "../components/Result"
 
 import { useState } from "react"
 
-import { BsImages } from "react-icons/bs"
-
 const MainPage = () => {
   const [file, setFile] = useState(null)
 
@@ -23,38 +21,7 @@ const MainPage = () => {
           پیش‌نمایش‌ ارائه می‌دهیم
         </p>
         <Dropzone file={file} setFile={setFile} />
-
-        <div className={styles.albumContainer}>
-          <div className={styles.albumTitle}>
-            <div className={styles.albumTitleContainer}>
-              <div className={styles.albumTitleIcon}>
-                <BsImages />
-              </div>
-              <div className={styles.albumInfo}>
-                <p>عکس های شما</p>
-                <p className={styles.albumInfoCount}>هنوز عکسی آپلود نشده</p>
-              </div>
-            </div>
-
-            <button>حذف همه</button>
-          </div>
-
-          <div className={styles.emptyResult}>
-            <div className={styles.emptyResultIcon}>
-              <BsImages />
-            </div>
-            <div className={styles.emptyResultText}>
-              <p className={styles.emptyResultTitle}>هنوز عکسی آپلود نشده</p>
-              <p>اولین عکستو به کمک بخش بالا آپلود کن</p>
-              <div className={styles.emptyResultFeatures}>
-                <p>✓ بگیر و رها کن</p>
-                <p>✓ انتخاب چندین عکس</p>
-                <p>✓ ردیابی فرایند</p>
-              </div>
-            </div>
-          </div>
-          {file && <Result file={file} setFile={setFile} />}
-        </div>
+        <Result />
       </div>
     </div>
   )
