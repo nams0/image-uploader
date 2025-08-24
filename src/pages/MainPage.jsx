e3b6a6c27e3c901fe2be1ebc73e99d7ae713f736
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-24 16:47:19 +0330

    changed state of files and initial state of it (empty array)

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,27 +1,24 @@
 import Dropzone from "../components/Dropzone"
-
 import styles from "./MainPage.module.css"
-
 import Navbar from "../components/Navbar"
 import Result from "../components/Result"
-
 import { useState } from "react"
 
 const MainPage = () => {
-  const [file, setFile] = useState(null)
+  const [files, setFiles] = useState([])
 
   return (
     <div>
       <Navbar />
       <div className={styles.container}>
-        <h2 className={styles.title}>عکس مورد نظرت رو آپلود کن</h2>
+        <h2 className={styles.title}>عکس‌های مورد نظرت رو آپلود کن</h2>
         <p className={styles.description}>
           تصاویر خود را بکشید و رها کنید یا برای انتخاب کلیک کنید. ما از همه
           قالب‌های رایج تصویر پشتیبانی می‌کنیم فرایند آپلود را به صورت آنی با
           پیش‌نمایش‌ ارائه می‌دهیم
         </p>
-        <Dropzone file={file} setFile={setFile} />
-        <Result />
+        <Dropzone files={files} setFiles={setFiles} />
+        <Result files={files} setFiles={setFiles} />
       </div>
     </div>
   )
