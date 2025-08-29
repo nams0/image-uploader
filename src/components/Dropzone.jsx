2787e3bc1c091effbbaae07cf0528bd0b2b77ee5
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-29 17:07:12 +0330

    lifted handleUpload function and it's imports dependencies up to MainPage to move upload button from under the dropzone to under the album container

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -2,17 +2,7 @@ import { useDropzone } from "react-dropzone"
 
 import { useDispatch, useSelector } from "react-redux"
 
-import {
-  setUploading,
-  setUploaded,
-  addFileUrl,
-  addDownloadUrl,
-  addFileInfo,
-  setError,
-  setUploadProgress,
-} from "../features/upload/uploadSlice"
-
-import api from "../services/api"
+import { setError } from "../features/upload/uploadSlice"
 
 import { LuUpload } from "react-icons/lu"
 import { CiImageOn } from "react-icons/ci"
@@ -23,7 +13,7 @@ import Cookies from "js-cookie"
 
 import { useNavigate } from "react-router-dom"
 
-function Dropzone({ files, setFiles }) {
+function Dropzone({ setFiles }) {
   const store = useSelector((store) => store.uploader)
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -56,68 +46,6 @@ function Dropzone({ files, setFiles }) {
       }
     },
   })
-
-  const handleUpload = async () => {
-    if (!files || files.length === 0) return
-    
-    const formData = new FormData()
-
-    dispatch(setUploading(true))
-    dispatch(setError(""))
-
-    // Upload all files
-    for (const file of files) {
-      // Skip if file is already uploaded
-      if (store.fileInfos.some((info) => info.name === file.name)) {
-        continue
-      }
-
-      formData.append("images", file)
-
-      try {
-        const response = await api.post("/api/images/upload", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `Bearer ${token}`,
-          },
-          onUploadProgress: (ProgressEvent) => {
-            const percentCompleted = Math.round(
-              (ProgressEvent.loaded * 100) / ProgressEvent.total
-            )
-            dispatch(
-              setUploadProgress({
-                fileName: file.name,
-                progress: percentCompleted,
-              })
-            )
-          },
-        })
-
-        console.log(response)
-        dispatch(addFileUrl(response.url))
-        dispatch(addDownloadUrl(response.downloadUrl))
-        dispatch(addFileInfo({ ...response.fileInfo, name: file.name }))
-      } catch (err) {
-        if (err.response) {
-          if (err.response.status === 429) {
-            dispatch(
-              setError(
-                "تعداد درخواست‌های شما زیاد بوده است. لطفاً یک ساعت دیگر تلاش کنید"
-              )
-            )
-          } else {
-            dispatch(setError("خطا در آپلود فایل: " + err.response.error))
-          }
-        } else {
-          dispatch(setError("خطا در اتصال به سرور"))
-        }
-        break // Stop uploading if there's an error
-      }
-    }
-
-    dispatch(setUploaded(true))
-    dispatch(setUploading(false))
-  }
 
   const clickHandler = () => {
     if (!token) navigate("/login")
@@ -156,10 +84,6 @@ function Dropzone({ files, setFiles }) {
       )}
 
       {store.error && <p className="error">{store.error}</p>}
-
-      <button onClick={handleUpload} disabled={store.uploading}>
-        {store.uploading ? "در حال آپلود..." : `آپلود ${files.length} عکس`}
-      </button>
     </div>
   )
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,92 @@
+import { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
 import Dropzone from "../components/Dropzone"
-import styles from "./MainPage.module.css"
 import Navbar from "../components/Navbar"
 import Result from "../components/Result"
-import { useState } from "react"
+
+import Cookies from "js-cookie"
+import api from "../services/api"
+
+import styles from "./MainPage.module.css"
+
+import {
+  setUploading,
+  setUploaded,
+  addFileUrl,
+  addDownloadUrl,
+  addFileInfo,
+  setError,
+  setUploadProgress,
+} from "../features/upload/uploadSlice"
 
 const MainPage = () => {
   const [files, setFiles] = useState([])
+  const store = useSelector((store) => store.uploader)
+  const dispatch = useDispatch()
+  const token = Cookies.get("auth-token")
+
+  const handleUpload = async () => {
+    if (!files || files.length === 0) return
+
+    const formData = new FormData()
+
+    dispatch(setUploading(true))
+    dispatch(setError(""))
+
+    // Upload all files
+    for (const file of files) {
+      // Skip if file is already uploaded
+      if (store.fileInfos.some((info) => info.name === file.name)) {
+        continue
+      }
+
+      formData.append("images", file)
+
+      try {
+        const response = await api.post("/api/images/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+          onUploadProgress: (ProgressEvent) => {
+            const percentCompleted = Math.round(
+              (ProgressEvent.loaded * 100) / ProgressEvent.total
+            )
+            dispatch(
+              setUploadProgress({
+                fileName: file.name,
+                progress: percentCompleted,
+              })
+            )
+          },
+        })
+
+        console.log(response)
+        dispatch(addFileUrl(response.url))
+        dispatch(addDownloadUrl(response.downloadUrl))
+        dispatch(addFileInfo({ ...response.fileInfo, name: file.name }))
+      } catch (err) {
+        if (err.response) {
+          if (err.response.status === 429) {
+            dispatch(
+              setError(
+                "تعداد درخواست‌های شما زیاد بوده است. لطفاً یک ساعت دیگر تلاش کنید"
+              )
+            )
+          } else {
+            dispatch(setError("خطا در آپلود فایل: " + err.response.error))
+          }
+        } else {
+          dispatch(setError("خطا در اتصال به سرور"))
+        }
+        break // Stop uploading if there's an error
+      }
+    }
+
+    dispatch(setUploaded(true))
+    dispatch(setUploading(false))
+  }
 
   return (
     <div>
@@ -17,8 +98,12 @@ const MainPage = () => {
           قالب‌های رایج تصویر پشتیبانی می‌کنیم فرایند آپلود را به صورت آنی با
           پیش‌نمایش‌ ارائه می‌دهیم
         </p>
-        <Dropzone files={files} setFiles={setFiles} />
+        <Dropzone setFiles={setFiles} />
         <Result files={files} setFiles={setFiles} />
+
+        <button onClick={handleUpload} disabled={store.uploading}>
+          {store.uploading ? "در حال آپلود..." : `آپلود ${files.length} عکس`}
+        </button>
       </div>
     </div>
   )
