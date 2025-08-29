54d0217ba7bf2d766a280b5d19b5e3129e7683fa
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-08-29 15:56:31 +0330

    -fixed a bug in accepting no image files -handled forwarding to login page on click and drag files when user is not logged in -fixed errors on rejected files on dropzone -fixed formData bug (in each loop new formData was created instead of appending new images to it)

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,5 +1,7 @@
 import { useDropzone } from "react-dropzone"
+
 import { useDispatch, useSelector } from "react-redux"
+
 import {
   setUploading,
   setUploaded,
@@ -9,29 +11,45 @@ import {
   setError,
   setUploadProgress,
 } from "../features/upload/uploadSlice"
+
 import api from "../services/api"
+
 import { LuUpload } from "react-icons/lu"
 import { CiImageOn } from "react-icons/ci"
+
 import styles from "./Dropzone.module.css"
+
+import Cookies from "js-cookie"
+
+import { useNavigate } from "react-router-dom"
 
 function Dropzone({ files, setFiles }) {
   const store = useSelector((store) => store.uploader)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const token = Cookies.get("auth-token")
 
   const { getRootProps, getInputProps } = useDropzone({
-    accept: "image/*",
+    accept: {
+      "image/*": [".jpeg", ".jpg", ".png", ".gif", ".bmp", ".webp"],
+    },
     maxSize: 3 * 1024 * 1024, // 3MB
     onDrop: (acceptedFiles) => {
+      if (!token) {
+        navigate("/login")
+        return
+      }
       dispatch(setError(""))
       console.log(acceptedFiles)
-      handleUpload()
+
       // Add new files to existing files
       setFiles((prevFiles) => [...prevFiles, ...acceptedFiles])
     },
     onDropRejected: (rejectedFiles) => {
-      if (rejectedFiles[0].size > 3 * 1024 * 1024) {
+      if (rejectedFiles[0].errors[0].code === "file-too-large") {
         dispatch(setError("حجم فایل نباید بیشتر از 3 مگابایت باشد"))
-      } else if (!rejectedFiles[0].type.startsWith("image/")) {
+      } else if (rejectedFiles[0].errors[0].code === "file-invalid-type") {
         dispatch(setError("فقط فایل‌های تصویری مجاز هستند"))
       } else {
         dispatch(setError("خطا در آپلود فایل"))
@@ -41,6 +59,8 @@ function Dropzone({ files, setFiles }) {
 
   const handleUpload = async () => {
     if (!files || files.length === 0) return
+    
+    const formData = new FormData()
 
     dispatch(setUploading(true))
     dispatch(setError(""))
@@ -52,13 +72,13 @@ function Dropzone({ files, setFiles }) {
         continue
       }
 
-      const formData = new FormData()
-      formData.append("image", file)
+      formData.append("images", file)
 
       try {
-        const response = await api.post("/upload", formData, {
+        const response = await api.post("/api/images/upload", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
           },
           onUploadProgress: (ProgressEvent) => {
             const percentCompleted = Math.round(
@@ -73,6 +93,7 @@ function Dropzone({ files, setFiles }) {
           },
         })
 
+        console.log(response)
         dispatch(addFileUrl(response.url))
         dispatch(addDownloadUrl(response.downloadUrl))
         dispatch(addFileInfo({ ...response.fileInfo, name: file.name }))
@@ -98,21 +119,47 @@ function Dropzone({ files, setFiles }) {
     dispatch(setUploading(false))
   }
 
+  const clickHandler = () => {
+    if (!token) navigate("/login")
+  }
+
   return (
     <div>
-      <div {...getRootProps()} className={styles.dropzone}>
-        <LuUpload className={styles.uploadIcon} />
-        <input {...getInputProps()} multiple />
-        <p className={styles.dropzoneText}>عکس‌ها را اینجا رها کنید</p>
-        <p>یا برای انتخاب کلیک کنید</p>
-        <div className={styles.selectButton}>
-          <CiImageOn strokeWidth={0.5} />
-          <p>انتخاب عکس‌ها</p>
+      {!token ? (
+        <div
+          {...getRootProps()}
+          className={styles.dropzone}
+          onClick={clickHandler}
+        >
+          <LuUpload className={styles.uploadIcon} />
+          <input {...getInputProps()} multiple />
+          <p className={styles.dropzoneText}>عکس‌ها را اینجا رها کنید</p>
+          <p>یا برای انتخاب کلیک کنید</p>
+          <div className={styles.selectButton}>
+            <CiImageOn strokeWidth={0.5} />
+            <p>انتخاب عکس‌ها</p>
+          </div>
+          <p className="limit-info">حداکثر حجم هر فایل: ۳ مگابایت</p>
         </div>
-        <p className="limit-info">حداکثر حجم هر فایل: ۳ مگابایت</p>
-      </div>
+      ) : (
+        <div {...getRootProps()} className={styles.dropzone}>
+          <LuUpload className={styles.uploadIcon} />
+          <input {...getInputProps()} multiple />
+          <p className={styles.dropzoneText}>عکس‌ها را اینجا رها کنید</p>
+          <p>یا برای انتخاب کلیک کنید</p>
+          <div className={styles.selectButton}>
+            <CiImageOn strokeWidth={0.5} />
+            <p>انتخاب عکس‌ها</p>
+          </div>
+          <p className="limit-info">حداکثر حجم هر فایل: ۳ مگابایت</p>
+        </div>
+      )}
 
       {store.error && <p className="error">{store.error}</p>}
+
+      <button onClick={handleUpload} disabled={store.uploading}>
+        {store.uploading ? "در حال آپلود..." : `آپلود ${files.length} عکس`}
+      </button>
     </div>
   )
 }
